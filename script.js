76a448bed56d115ad832d18a1dafae0ee65826c9
yenvy <vynguyn22@gmail.com>
2023-09-26 16:04:11 +0700

    Bootstrap JQuery

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-<script>
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
         $(document).ready(function(){
-            $('[data-toggle="tooltip"]').tooltip()
+            $("#mycarousel").carousel( { interval: 2000 } );
+            $("#carousel-pause").click(function(){
+                $("#mycarousel").carousel('pause');
+            });
+            $("#carousel-play").click(function(){
+                $("#mycarousel").carousel('cycle');
+            });
         });
-</script>
+
+
+
